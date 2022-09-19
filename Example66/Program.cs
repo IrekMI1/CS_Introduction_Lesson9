679d3dc30373fe91d5a3ff0a6246dfc352c75b51
Irek <general157@mail.ru>
2022-09-19 22:53:56 +0300

    add example 66

---
+++ Example66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetRangeSum(int m, int n)
+{
+    if (m == n) return n;
+    else return m + GetRangeSum(m + 1, n);
+}
+
+try
+{
+    Console.Write("Введите первое число:\t");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число:\t");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    if (number1 > number2) Console.WriteLine("Первое число больше, чем второе!");
+    else Console.WriteLine("Сумма элементов от {0} до {1} равно {2}", number1, number2, GetRangeSum(number1, number2));
+}
+catch 
+{
+    Console.WriteLine("Неверный ввод!");
+}
